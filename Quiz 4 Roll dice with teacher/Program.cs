db74becd39b6f15723ec50219daf7092773cc6fc
Ashton Pate <arpate@amarillocollege.com>
2025-02-06 13:07:22 -0600

    Add project files.

---
+++ Quiz 4 Roll dice with teacher/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz_4_Roll_dice_with_teacher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
